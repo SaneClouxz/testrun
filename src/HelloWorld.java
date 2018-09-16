92d373772eeedfd26ed609f9b3713ce5e9437266
HP <HP@Clouxz>
2018-09-16 14:45:23 -0700

    Test push

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+	public int method1() {
+		int a = 10;
+		int b = 7;
+		int c = 0;
+		
+		if (b >= a)
+			c++;
+		b = -47;
+		if (b > c)
+			c = b;
+		
+		return c;
+	}
+	}
+
+
---
+++ src/HelloWorldTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void testMethod1() {
+		fail("Not yet implemented");
+	}
+
+}
